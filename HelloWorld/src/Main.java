59c1a40357b0d477a052dc9d666d88b005ecbf37
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-07-08 21:19:22 -0300

    Hello World

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        System.out.print("Cat\n");
+        System.out.println("Big\tDog");
+        System.out.println("Rabbit Delete\b");
+
+        System.out.println("The dog said \"Woof\"");
+
+        System.out.println("C:\\some\\path\\big\\name");
+    }
+}
