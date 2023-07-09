c7c2f758f3594e95dbc6cfd8b2d759235f214814
Nathália Pissuti <nathaliapissuti@gmail.com>
2023-07-08 21:52:39 -0300

    Type casting

---
+++ HelloWorld/src/unit4_variables/TypeCasting.java
@@ -0,0 +1,13 @@
+package unit4_variables;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        double foo = 5.5;
+        int bar = (int) foo;
+
+        int n1 = 17;
+        int n2 = 4;
+
+        System.out.println((double) n1 / n2);
+    }
+}
--- HelloWorld/src/unit4_variables/Variables.java
+++ HelloWorld/src/unit4_variables/Variables.java
@@ -1,6 +1,6 @@
 package unit4_variables;
 
-public class Main {
+public class Variables {
     public static void main(String[] args) {
         int age = 10;
         double money = 10.5;
